23e8ab62922d43eaef91fe6120b7ed74171983ab
coblu3 <demirel.akif@hotmail.com>
2022-02-25 05:01:41 +0300

    recognitionEklendi

---
+++ textRecognition.py
@@ -0,0 +1,45 @@
+import os
+import cv2
+import numpy as np
+from textRecognitionModel import Model
+import keras
+
+char_list = ['#', '%', "'", '(', ')', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '=', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ı']
+
+
+def recognition(path="yolo_crop"):
+    images = []
+    for i in os.listdir("yolo_crop"):
+        img = cv2.imread(path+"/"+i,cv2.COLOR_RGB2BGR)
+        img = cv2.resize(img,(128,32))
+        images.append(img)
+    
+
+    model = Model()
+    model.load_weights("modelWeigght")
+
+
+    prediction_model = keras.models.Model(
+        model.get_layer(name="image").input, model.get_layer(name="dense").output
+        )
+
+
+
+    preds = prediction_model.predict(images.reshape(-1,32,128,1),batch_size=32)
+    input_len = np.ones(preds.shape[0]) * preds.shape[1]
+    results = keras.backend.ctc_decode(preds, input_length=input_len, greedy=True)[0][0]
+    pred_texts = []
+
+    for res in results.numpy():
+        res = "".join([char_list[i] for i in res if i != -1 and i < len(char_list)])
+        res = res.lower()
+        pred_texts.append(res)
+
+
+    for i in range(len(images)):
+        pred = pred_texts[i].replace("[UNK]","")
+        with open("preds.txt","a+") as f:
+            f.write(pred+"\n")
+
+
+
---
+++ textRecognitionModel.py
@@ -0,0 +1,123 @@
+import os
+import cv2
+import numpy as np
+import string
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+import tensorflow.keras.backend as K
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Dense, Reshape, BatchNormalization, Input, Conv2D, MaxPool2D, Lambda, Bidirectional
+from tensorflow.compat.v1.keras.layers import CuDNNLSTM
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.utils import to_categorical, Sequence
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+
+char_list = ['#', '%', "'", '(', ')', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '=', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ı']
+
+
+class CTCLayer(layers.Layer):
+
+    def __init__(self, name=None):
+
+        super().__init__(name=name)
+        self.loss_fn = keras.backend.ctc_batch_cost
+
+    def call(self, y_true, y_pred):
+        # Compute the training-time loss value and add it
+        # to the layer using `self.add_loss()`.
+
+        batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+        input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+        label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+
+        input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+        label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+
+        loss = self.loss_fn(y_true, y_pred, input_length, label_length)
+        self.add_loss(loss)
+
+        # At test time, just return the computed predictions
+        return y_pred
+
+
+def Model(epochs=0):
+    
+    # input with shape of height=32 and width=128 
+    inputs = Input(shape=(32, 128, 1), name="image")
+
+    labels = layers.Input(name="label", shape=(None,), dtype="float32")
+
+    conv_1 = Conv2D(32, (3,3), activation = "selu", padding='same')(inputs)
+    pool_1 = MaxPool2D(pool_size=(2, 2))(conv_1)
+    
+    conv_2 = Conv2D(64, (3,3), activation = "selu", padding='same')(pool_1)
+    pool_2 = MaxPool2D(pool_size=(2, 2))(conv_2)
+
+    conv_3 = Conv2D(128, (3,3), activation = "selu", padding='same')(pool_2)
+    conv_4 = Conv2D(128, (3,3), activation = "selu", padding='same')(conv_3)
+
+    pool_4 = MaxPool2D(pool_size=(2, 1))(conv_4)
+    
+    conv_5 = Conv2D(256, (3,3), activation = "selu", padding='same')(pool_4)
+    
+    # Batch normalization layer
+    batch_norm_5 = BatchNormalization()(conv_5)
+    
+    conv_6 = Conv2D(256, (3,3), activation = "selu", padding='same')(batch_norm_5)
+    batch_norm_6 = BatchNormalization()(conv_6)
+    pool_6 = MaxPool2D(pool_size=(2, 1))(batch_norm_6)
+    
+    conv_7 = Conv2D(64, (2,2), activation = "selu")(pool_6)
+    
+    squeezed = Lambda(lambda x: K.squeeze(x, 1))(conv_7)
+    
+    # bidirectional LSTM layers with units=128
+    blstm_1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(squeezed)
+    blstm_2 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(blstm_1)
+
+    
+    softmax_output = Dense(len(char_list) + 1, activation = 'softmax', name="dense")(blstm_2)
+
+    output = CTCLayer(name="ctc_loss")(labels, softmax_output)
+
+
+    optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, clipnorm=1.0)
+
+    
+
+    #model to be used at training time
+    model = Model(inputs=[inputs, labels], outputs=output)
+    #model.load_weights('/content/TextRecognition_best.hdf5')
+    model.compile(optimizer = optimizer)
+    
+
+    file_path = "TextRecognition_lower_Char_best.hdf5"
+    
+    checkpoint = ModelCheckpoint(filepath=file_path, 
+                                monitor='val_loss', 
+                                verbose=1, 
+                                save_best_only=True, 
+                                mode='min')
+
+    callbacks_list = [checkpoint, 
+                      #PlotPredictions(frequency=1),
+                      #EarlyStopping(patience=3, verbose=1)
+    ]
+
+    history = model.fit(#train_dataset, 
+                        epochs = epochs,
+                        #validation_data=validation_dataset,
+                        verbose = 1,
+                        callbacks = callbacks_list,
+                        shuffle=True)
+
+    
+    
+    return model
